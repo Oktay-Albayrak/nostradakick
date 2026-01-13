eea2e855e38b3b479a492488f03317e7c0840bdb
Albayrak <albayrak.ok@gmail.com>
2026-01-13 18:10:12 +0100

    feature api getOneUser is ok

--- api/src/routers/index.routes.ts
+++ api/src/routers/index.routes.ts
@@ -1,6 +1,6 @@
 import { Router } from "express";
-import { router as matchesRouter } from "./match.routes.ts";
-import { router as usersRouter } from "./users.routes";
+// import { router as matchesRouter } from "./match.routes.ts";
+import { router as usersRouter } from "./users.routes.ts";
 import { healthCheck } from "../controllers/main.controller.ts";
 
 export const router: Router = Router();
