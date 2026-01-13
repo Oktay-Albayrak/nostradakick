365d97f568420f01dff860d4b1c64eec88836a1f
Benjamin RENEE <benjamin.renee@oclock.school>
2026-01-13 16:10:54 +0100

    fix layout conflict #2

--- client/src/app/layout.tsx
+++ client/src/app/layout.tsx
@@ -16,7 +16,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={`${geistSans.variable} ${geistMono.variable} ${ubuntu.variable}`}>
+      <body>
         <Header />
           {children}
         <Footer />
