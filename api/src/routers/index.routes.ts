55abbcd706886bbe3fe295843daa23b5889a14a8
Hakim dublin <hakim.moussouni@oclock.school>
2026-01-13 18:13:44 +0100

    feat: GET /matches and /matches/:api_id done

--- api/src/routers/index.routes.ts
+++ api/src/routers/index.routes.ts
@@ -1,6 +1,6 @@
 import { Router } from "express";
-import { router as matchesRouter } from "./match.routes.ts";
-import { router as usersRouter } from "./users.routes";
+import { router as matchesRouter } from "./matches.routes.ts";
+import { router as usersRouter } from "./users.routes.ts";
 import { healthCheck } from "../controllers/main.controller.ts";
 
 export const router: Router = Router();
@@ -9,8 +9,4 @@ router.get("/health", healthCheck);
 
 // Branchement des routeurs
 router.use(usersRouter);
-
-
-
-
-
+router.use(matchesRouter);
