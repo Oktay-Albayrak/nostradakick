84f9c2764f4886ae4b02f26e6630e5f08a26d0e9
Albayrak <albayrak.ok@gmail.com>
2026-01-14 10:53:20 +0100

    ajout de commentaire

--- api/src/controllers/users.controller.ts
+++ api/src/controllers/users.controller.ts
@@ -23,13 +23,18 @@ export async function getAllUsers(req: Request, res: Response) {
 // Fonction permettant de récupérer un utilisateur via son 'username'
 export async function getOneUser(req: Request, res: Response) {
 
+    // on définit un schéma de validation pour le username (doit être une chaîne de caractères valide)
+    // afin d'éviter des erreurs avec TypeScript et Prisma
     const usernameSchema = z.string()
-    .min(3)
-    .max(50)
-    .regex(/^[a-zA-Z0-9_]+$/);
+    .min(3) // minimum 3 caractères
+    .max(50) // maximum 50 caractères
+    .regex(/^[a-zA-Z0-9_]+$/); // caractères autorisés
 
+    // On valide le username avec le schéma Zod. Si la validation échoue, on retourne une erreur 400
     const parseResult = usernameSchema.safeParse(req.params.username);
 
+
+    // ce message d'erreur apparaîtra seulement si le pseudo est trop court ou trop long ou contient des caractère non autorisé
     if (!parseResult.success) {
     return res.status(400).json({
         error: "Username invalide",
@@ -37,8 +42,11 @@ export async function getOneUser(req: Request, res: Response) {
     });
     }
 
+    // si validation réussi on récupère le username validé de la base de donnée
     const username = parseResult.data; 
 
+
+    // on recherche dans la base de donné pour montré seulement 1 utilisateur en excluant les champs sensibles (password_hash email)
     const user = await prisma.user.findUnique({
     where: { username },
     omit: { 
@@ -46,9 +54,11 @@ export async function getOneUser(req: Request, res: Response) {
         email: true },
     });
 
+    // si l'utilisateur n'existe pas on renvoie une 404
     if (!user) {
     return res.status(404).json({ error: "Utilisateur non trouvé" });
     }
 
+    // on retourne les données au format JSON
     res.json(user);
 }
