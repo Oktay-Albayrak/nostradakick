d7cc6f85cf9d1590589f0652b506d9105a05007a
Benjamin RENEE <benjamin.renee@oclock.school>
2026-01-14 11:16:30 +0100

    add comments

--- api/src/controllers/predictions.controller.ts
+++ api/src/controllers/predictions.controller.ts
@@ -6,10 +6,9 @@ export async function getAllPredictions(req: Request, res: Response) {
   const predictions = await prisma.prediction.findMany({
     include: {
       match: {
-        omit: { api_id: true },
-        include: { home_team: true, away_team: true, competition: true }
+        include: { home_team: true, away_team: true, competition: true } // Include pour faire appel a la relation et avoir les détails des matchs grace aux ids
       },
-      user: { select: { id: true, username: true, avatar_url: true } }
+      user: { select: { id: true, username: true, avatar_url: true } } // Sélectionner seulement les infos publics
     }
   });
   res.json(predictions);
