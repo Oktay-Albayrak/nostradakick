17a5f5e45f173bc96ed576ac3e80ecad797189f8
Hakim dublin <hakim.moussouni@oclock.school>
2026-01-15 09:52:36 +0100

    feat: card structure

--- client/src/components/matchCard/MatchCard.tsx
+++ client/src/components/matchCard/MatchCard.tsx
@@ -1,7 +1,37 @@
+import Image from "next/image";
+
 export default function MatchCard() {
   return (
     <div>
-      <h2>test MatchCard</h2>
+      <article>
+        <section>
+          <div>Competition</div>
+          <span>🔥</span>
+          <div>
+            <p>Date</p>
+            <p>Heure</p>
+          </div>
+        </section>
+        <section>
+          <Image
+            src="https://crests.football-data.org/524.png"
+            alt="logo_paris-saint-germain"
+            width={64}
+            height={64}
+          />
+          <Image
+            src="https://crests.football-data.org/516.png"
+            alt="logo_olympique-marseille"
+            width={64}
+            height={64}
+          />
+        </section>
+        <section>
+          <button>Paris Saint-Germain</button>
+          <button>Match Nul</button>
+          <button>Olympique de Marseille</button>
+        </section>
+      </article>
     </div>
   );
 }
