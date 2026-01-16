86aa9875522738b7a1cb169b318d6443c9889f35
Albayrak <albayrak.ok@gmail.com>
2026-01-16 15:26:32 +0100

    Refactoring: getAllUser and file: service

--- api/src/user_services/deleteOneUser.service.ts
+++
@@ -1,8 +0,0 @@
-import { prisma } from "../lib/prisma.ts";
-
-// Suppression de l'utilisateur de la bdd
-export async function deleteUser(id: string) {
-  await prisma.user.delete({
-    where: { id },
-  });
-}
