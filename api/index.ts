39b8a3d9a862b183c6103651a13fcdf812abc4dd
Albayrak <albayrak.ok@gmail.com>
2026-01-19 17:08:05 +0100

    fix:  modification cronJob + ajout cronJob pour récupérer les country(compétition etc) + modification api.http et index.ts

--- api/index.ts
+++ api/index.ts
@@ -1,20 +1,25 @@
 import { app } from "./src/app.ts";
 import { config } from "./config.ts";
 import "./src/jobs/syncMatches.cronJob.ts"
+import "./src/jobs/syncCompetitions.cronJob.ts"
 
 import {
   syncAllCompetitions,
+  syncAllMatches,
   syncMatchesForCompetition,
 } from "./src/services/sync.service.ts";
 
-// Route temporaire pour tester le Token et la synchro
-app.get("/api/admin/test-sync-all", async (req, res) => {
+
+
+// Route de test pour synchroniser TOUTES les compétitions manuellement
+app.get("/api/admin/test-sync-competitions", async (req, res) => {
   try {
     // On teste avec toutes les compétitions
     await syncAllCompetitions();
     res.json({
       message:
-        "Synchro réussie ! Vérifie ton interface client ou Prisma Studio.",
+        "Synchro réussie !",
+      info: "Vérifie ton interface client ou Prisma Studio."
     });
   } catch (error: any) {
     res.status(500).json({
@@ -24,13 +29,38 @@ app.get("/api/admin/test-sync-all", async (req, res) => {
   }
 });
 
-app.get("/api/admin/test-sync-one", async (req, res) => {
+
+// Route de test pour synchroniser TOUS les matchs manuellement
+app.get("/api/admin/test-sync-matches", async (req, res) => {
   try {
+    await syncAllMatches();
+    res.json({
+      message: "Synchro des matchs réussie !",
+      info: "6 ligues synchronisé en parallèle"
+    });
+  } catch (error: any) {
+    res.status(500).json({
+      error: "La synchro a échoué",
+      detail: error.message,
+    });
+  }
+})
+
+
+// Route de test pour synchroniser UNE SEULE ligue manuellement
+// exemple: /api/admin/test-sync-one?league=CL
+app.get("/api/admin/test-sync-one", async (req, res) => {
+  
+  try {
+    const league = (req.query.league as string) || "CL";
+
     // On teste avec une compétition
-    await syncMatchesForCompetition("CL");
+    await syncMatchesForCompetition(league);
+
     res.json({
       message:
-        "Synchro réussie ! Vérifie ton interface client ou Prisma Studio.",
+        `Synchro de ${league} réussie !`,
+      info: "Vérifie ton interface client ou Prisma Studio."
     });
   } catch (error: any) {
     res.status(500).json({
