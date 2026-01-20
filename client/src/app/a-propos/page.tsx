6ac32f9ac2e5f12a736652be87e1bbd5a0fb1e81
Lanh Oclock <willanh.luangphavy@oclock.school>
2026-01-20 09:29:54 +0100

    fix echap l'

--- client/src/app/a-propos/page.tsx
+++ client/src/app/a-propos/page.tsx
@@ -11,14 +11,14 @@ export default function AProposPage() {
           <p className={styles.paragraph}>
             NostradaKick est une plateforme web communautaire dédiée aux passionnés
             de football. Elle permet de consulter les matchs à venir, de soumettre
-            des pronostics sur des rencontres réelles et de suivre l'évolution de
+            des pronostics sur des rencontres réelles et de suivre l&apos;évolution de
             ses performances au fil du temps.
           </p>
 
           <p className={styles.paragraph}>
-            L'objectif de la plateforme est de proposer un environnement clair et
-            accessible favorisant l'analyse, la comparaison des pronostics et
-            l'échange entre utilisateurs autour des compétitions sportives.
+            L&apos;objectif de la plateforme est de proposer un environnement clair et
+            accessible favorisant l&apos;analyse, la comparaison des pronostics et
+            l&apos;échange entre utilisateurs autour des compétitions sportives.
           </p>
         </section>
       </section>
