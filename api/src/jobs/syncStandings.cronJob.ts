1d9a3100ea74d6af790fb6a733815ffcea573726
Oktay Albayrak <albayrak.ok@gmail.com>
2026-05-31 17:51:14 +0200

    fix: delay statup sync to allow Render boot time

--- api/src/jobs/syncStandings.cronJob.ts
+++ api/src/jobs/syncStandings.cronJob.ts
@@ -24,15 +24,15 @@ const standingsCronJob = new CronJob(
 standingsCronJob.start();
 
 
-console.log('⏳ Synchronisation des standing prévue 2 minutes...');
-// Au démarrage du serveur, on l'exécute après 2 minutes
-// (Les matchs sont à 0 min, les compétitions à 1 min, les métadonnées à 3 min)
+console.log('⏳ Synchronisation des standing prévue 4 minutes...');
+// Au démarrage du serveur, on l'exécute après 4 minutes
+// (Les matchs sont à 1 min, les compétitions à 2 min, les métadonnées à 3 min)
 setTimeout(
   () => {
     console.log("📊 Lancement de la synchronisation initiale des Standings...");
     syncStandings().catch((err) => console.error(err));
   },
-  3 * 60 * 1000,
+  4 * 60 * 1000, // 4 minutes
 );
 
 console.log("📊 Cron Job Standings démarré - Toutes les 3h à la minute 10");
